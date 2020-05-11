55ee74d3bf7ffe0a90888a92d5e07fc3b46dbc78
user <mobiskif@github.io>
2020-05-11 23:56:33 +0300

    11.05.20

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        Socket cs;
         try {
-            if (args.length==0) cs = new Socket("localhost", 1234);
-            else cs = new ServerSocket(1234).accept();
-            new SocketHelper(cs);
+            if (args.length==0) new SocketHelper (new Socket("localhost", 1234));
+            else new SocketHelper(new ServerSocket(1234).accept());
         }
         catch (IOException e) {e.printStackTrace();}
     }
--- src/SocketHelper.java
+++ src/SocketHelper.java
@@ -2,19 +2,19 @@ import java.io.*;
 import java.net.Socket;
 
 public class SocketHelper {
-    Socket socket;
+    //Socket socket;
     boolean stop_request = false;
     public SocketHelper(Socket s) {
         System.out.println(s);
-        socket = s;
+        //socket = s;
         try {
             PrintWriter net_out = new PrintWriter(s.getOutputStream(), true);
             BufferedReader net_in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             PrintWriter std_out = new PrintWriter(System.out, true);
             BufferedReader std_in = new BufferedReader(new InputStreamReader(System.in));
 
-            from_to(net_in, std_out);
-            from_to(std_in, net_out);
+            //from_to(net_in, std_out);
+            //from_to(std_in, net_out);
         }
         catch (IOException e) {e.printStackTrace();}
     }
@@ -32,7 +32,7 @@ public class SocketHelper {
                     }
                     in.close();
                     out.close();
-                    socket.close();
+                    //socket.close();
                 }
                 catch (IOException e) {
                     //e.printStackTrace();

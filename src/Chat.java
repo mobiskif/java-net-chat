e3020e2b89ad72d450b768cd326ff57c8962e649
user <mobiskif@github.io>
2020-05-15 12:30:04 +0300

    14.05.20

--- src/Chat.java
+++ src/Chat.java
@@ -1,13 +1,14 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 
 public class Chat {
     boolean stop =false; //нужно для уведомления второго потока, при остановке первого
-    final Socket socket;
+    //final Socket socket;
 
     public Chat(Socket s) {
-        socket = s;
+        //socket = s;
         try {
             PrintWriter net_out = new PrintWriter(s.getOutputStream(), true);
             BufferedReader net_in = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -17,16 +18,30 @@ public class Chat {
             transferThread(net_in, std_out, "from_net").start();
             transferThread(std_in, net_out, "to_net").start();
 
+            URL url = new URL("https://mobiskif.github.io/hh_JAVA/");
+            BufferedReader url_in = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), "UTF-8"));
+            transferUrlThread(url_in, std_out, "from_url").start();
+
         } catch (IOException e) { e.printStackTrace();}
     }
 
+    private Thread transferUrlThread(BufferedReader in, PrintWriter out, String msg) {
+        return new Thread(() -> {
+            String readLine;
+            try {
+                while ((readLine = in.readLine()) != null) out.println("\t" + readLine);
+                in.close();
+            }
+            catch (IOException ignored) { }
+        });
+    }
 
     private Thread transferThread(BufferedReader in, PrintWriter out, String msg) {
         return new Thread(() -> {
             String readLine;
             try {
                 while ((readLine = in.readLine()) != null && !stop) out.println("\t" + readLine);
-                socket.close();
+                in.close();
             }
             catch (IOException ignored) { }
             stop=true;
